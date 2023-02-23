cc97b0c11e896f9bfabebf5c4fae75ef1a6e06f9
hannabodnar <126188913+hannabodnar@users.noreply.github.com>
2023-02-23 16:02:27 -0500

    Update tv_shows.sql

--- tv_shows.sql
+++ tv_shows.sql
@@ -1,3 +1,6 @@
+-- This query is used to answer the question, "Which genre had the most releases?"
+-- Note this query is specific to 'TV Shows'
+
 SELECT listed_in, COUNT(listed_in)
 FROM netflix_condense
 WHERE country = 'United States' AND type = 'TV Show'
