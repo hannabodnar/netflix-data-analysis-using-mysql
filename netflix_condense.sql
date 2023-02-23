8addb5fb5ab2f448d7725a3f7cd20d6bcb251202
hannabodnar <126188913+hannabodnar@users.noreply.github.com>
2023-02-23 14:35:40 -0500

    Add files via upload

---
+++ netflix_condense.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `netflix_condense` (
+  `type` varchar(45) NOT NULL,
+  `title` varchar(1000) NOT NULL,
+  `country` varchar(1000) NOT NULL,
+  `date_added` text,
+  `release_year` int NOT NULL,
+  `rating` varchar(45) NOT NULL,
+  `duration` varchar(45) NOT NULL,
+  `listed_in` varchar(1000) NOT NULL,
+  `show_id` varchar(45) NOT NULL,
+  PRIMARY KEY (`show_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
