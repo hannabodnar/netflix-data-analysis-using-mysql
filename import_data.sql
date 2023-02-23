c5ea43f75e5cb662eed63cdbb60ff11a80cb5d72
hannabodnar <126188913+hannabodnar@users.noreply.github.com>
2023-02-23 15:13:07 -0500

    Update import_data.sql

--- import_data.sql
+++ import_data.sql
@@ -1,3 +1,5 @@
+-- After creating my condensed table, I imported the information I needed from my original table. 
+
 INSERT INTO netflix_condense (show_id, type, title, country, date_added, release_year, rating, duration, listed_in)
 SELECT show_id, type, title, country, date_added, release_year, rating, duration, listed_in
 FROM netflix_titles;
