9a5c792441a28b4093c751cbea7145a8666c6f1c
hannabodnar <126188913+hannabodnar@users.noreply.github.com>
2023-02-23 15:32:44 -0500

    Update category.sql

--- category.sql
+++ category.sql
@@ -2,4 +2,4 @@ SELECT listed_in, COUNT(listed_in)
 FROM netflix_condense
 WHERE country = 'United States' AND type = 'Movie'
 GROUP BY listed_in
-HAVING COUNT(listed_in) > 1;
+ORDER BY COUNT(listed_in) DESC;
