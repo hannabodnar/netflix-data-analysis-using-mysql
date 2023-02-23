c5ed169739a30af150040d4372f5d04c72b147e5
hannabodnar <126188913+hannabodnar@users.noreply.github.com>
2023-02-23 15:45:54 -0500

    Update category.sql

--- category.sql
+++ category.sql
@@ -1,3 +1,7 @@
+-- This query is used to answer the question, "Which genre had the most releases?"
+-- Note this query is specific to 'Movies'
+
+
 SELECT listed_in, COUNT(listed_in)
 FROM netflix_condense
 WHERE country = 'United States' AND type = 'Movie'
