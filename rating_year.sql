a90406117ef839ff24a037eee60eff4b58f788a9
hannabodnar <126188913+hannabodnar@users.noreply.github.com>
2023-02-24 22:14:49 -0500

    Delete rating_year.sql

--- rating_year.sql
+++
@@ -1,9 +0,0 @@
-SELECT release_year, rating, type, COUNT(rating) AS rating_amount
-FROM netflix_condense
-WHERE country = 'United States' 
-GROUP BY release_year, rating, type
-ORDER BY release_year DESC, COUNT(rating) DESC;
-
-
-
-
