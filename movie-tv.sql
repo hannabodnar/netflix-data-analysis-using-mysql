353924384903090173580e081b62a4ef1a8f5cff
hannabodnar <126188913+hannabodnar@users.noreply.github.com>
2023-02-24 22:15:17 -0500

    Delete movie-tv.sql

--- movie-tv.sql
+++
@@ -1,7 +0,0 @@
---Answers the question, 'In the United States, were there more movies or TV shows released?'
-
-SELECT type, COUNT(type)
-FROM netflix_condense
-WHERE country = 'United States'
-GROUP BY type
-HAVING COUNT(type) > 1;
