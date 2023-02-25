3f73849a3e7634175d0cc342152e383ee8c5d9b6
hannabodnar <126188913+hannabodnar@users.noreply.github.com>
2023-02-24 22:14:16 -0500

    Delete category.sql

--- category.sql
+++
@@ -1,9 +0,0 @@
--- This query is used to answer the question, "Which genre had the most releases?"
--- Note this query is specific to 'Movies'
-
-
-SELECT listed_in, COUNT(listed_in)
-FROM netflix_condense
-WHERE country = 'United States' AND type = 'Movie'
-GROUP BY listed_in
-ORDER BY COUNT(listed_in) DESC;
