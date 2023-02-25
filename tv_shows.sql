8f70f4181dbd25db6f787a84d2c177ed431f1c67
hannabodnar <126188913+hannabodnar@users.noreply.github.com>
2023-02-24 22:14:38 -0500

    Delete tv_shows.sql

--- tv_shows.sql
+++
@@ -1,8 +0,0 @@
--- This query is used to answer the question, "Which genre had the most releases?"
--- Note this query is specific to 'TV Shows'
-
-SELECT listed_in, COUNT(listed_in)
-FROM netflix_condense
-WHERE country = 'United States' AND type = 'TV Show'
-GROUP BY listed_in
-ORDER BY COUNT(listed_in) DESC;
